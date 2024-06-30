332fd32943f02d82f8c0eaace1c49435d8ebc688
sudip <sudipkandel2056@gmail.com>
2024-06-30 21:55:28 +0545

    complete  framework

---
+++ StudentPortol.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortol.Web.Models.Entities;
+
+namespace StudentPortol.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Student> Students{ get; set; }
+    }
+}
---
+++ StudentPortol.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentPortol.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
